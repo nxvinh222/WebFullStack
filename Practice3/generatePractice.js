26b5f5930fd611b1e28cc54ca66547a47a1696e8
nxvinh222 <nxvinh222@gmail.com>
2019-03-29 22:54:54 +0700

    fix

--- Practice3/generatePractice.js
+++ Practice3/generatePractice.js
@@ -32,10 +32,35 @@ function generate(testLengthArray){
     };
     for (let j = 0; j < testLengthArray[i]; j++){
       //console.log(testLengthArray[i]);
-      item.input.push(getRandomInt(-10, 10));
+      item.input.push(getRandomInt(-10000, 10000));
     }
-    item.target = (getRandomInt(-10, 10));
-    item.output = search(item.input, item.target);
+
+    if (i == 0){
+      item.target = 10001;
+      item.output = -1;
+    }
+    else if (i == 1){
+      item.target = item.input[0];
+      item.output = 0;
+    }
+    else if (i == 2){
+      item.target = item.input[item.input.length-1];
+      item.output = item.input.length-1;
+    }
+    else if (i == 3){
+      item.target = item.input[2];
+      item.output = 2;
+    }
+    else {
+      item.target = getRandomInt(-10000, 10000);
+      item.output = search(item.input, item.target);
+    }
+
+
+
+    //item.target = (getRandomInt(-10000, 10000));
+    //item.output = search(item.input, item.target);
+
 
     object.push(item);
   }

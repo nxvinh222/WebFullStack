61bbeb8f9ed6b8b2a69183cfa7d560b8dfe837d3
unknown <nxvinh222@gmail.com>
2019-04-20 11:25:41 +0700

    upload

--- code/quyet de/server.js
+++ code/quyet de/server.js
@@ -2,6 +2,33 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser")
 const fs = require("fs");
+const mongoose = require("mongoose");
+
+const QuestionModel = require("./models/questionModel")
+
+
+mongoose.connect("mongodb://localhost/quyet-de-21", 
+    { useNewUrlParser: true},
+    function(err){
+        if (err) console.log(err)
+        else console.log("DB connected");
+
+        // QuestionModel.create({
+        //     content: "Hello World",
+
+        // }, function(err, docCreated){
+        //     if (err) console.log(err)
+        //     else console.log("Created!");
+        // })
+
+        QuestionModel.find({}, function(err, docs){
+            if (err) console.log(err)
+            else console.log("Question: ", docs);
+        })
+    }
+)
+
+
 
 // data-type: application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false}))
@@ -39,12 +66,43 @@ app.get("/", function(req, res){
 
 app.get("/randomquestion", function(req, res){
 
-    const questions = JSON.parse(
-        fs.readFileSync("questions.json", {encoding:"utf-8"}
-        ));
-    const randomId = Math.floor(Math.random()*questions.length);
-    const question = questions[randomId];
-    res.send(question);
+    //Cach 1
+    // QuestionModel.count({}, function(err, count){
+    //     if (err) console.log(err)
+    //     else QuestionModel.find({}, function(err, docs){
+    //         if (err) console.log(err)
+    //         else res.send(docs[Math.floor(Math.random()*count)]);
+    //     })
+      
+    // });
+
+    //Cach 1.5
+    // QuestionModel.find({}, function(err, docs){
+    //     if (err) console.log(err)
+    //     else {
+    //         const randomIndex = Math.floor(Math.random()*docs.length)
+    //         const question = docs[randomIndex];
+    //         // res.send(question);
+    //         res.json(question);
+    //     }
+    // })
+
+    //Cach 2
+    QuestionModel.count({}, function(err, count){
+        if (err) console.log(err)
+        else {
+            const randomId = Math.floor(Math.random()*count);
+            QuestionModel
+                .findOne({})
+                .skip(randomId)
+                .exec(function(err, question){
+                    if (err) console.log(err)
+                    else res.json(question)
+                })
+        }
+    })
+  
+    
 
 })
 
@@ -69,22 +127,19 @@ app.get("/votes/:id", function(req, res){
 })
 
 app.post("/addquestion", function(req, res){
-    // lay question tu req.body
-    // const question = req.body.question
-    const questions = JSON.parse(
-        fs.readFileSync("./questions.json", { encoding: "utf-8" })
-    );
+    
     const { question } = req.body;
-    const newQuestion = {
-        content: question,
-        yes: 0,
-        no: 0,
-        id: questions.length,
-    }
-    questions.push(newQuestion);
-    fs.writeFileSync("./questions.json", JSON.stringify(questions));
+    QuestionModel.create({
+            content: question,
+
+        }, function(err, docCreated){
+            if (err) console.log(err)
+            else console.log("Created!");
+        })
+
+
     res.send(`<a href="/">Go back</a>`);
-    // res.send("ok");
+
 })
 
 //http://localhost:9696/test?ids=3&answer=1
@@ -98,19 +153,15 @@ app.get('/test', function(req, res){
         ));
     
 
-    
+    // QuestionModel.find({}, function(err, docs){
+    //     if 
+    // })
+
     questions[ids][answer]++
     console.log(ids);
     console.log(answer);
     console.log(questions[ids][answer])
-    //  for (let i=0 ; i < questions.length ; i++)
-    //  {
-    //      if (questions[i].id == ids) {
-    //             questions[i].[vote]++;
-    //             // res.send(`<li>yes: ${questions[i].yes}<li>
-    //             //           <li>no: ${questions[i].no}<li>`);
-    //     }
-    // }
+   
 
 
     fs.writeFileSync("./questions.json", JSON.stringify(questions));
@@ -118,19 +169,6 @@ app.get('/test', function(req, res){
     
 })
 
-// app.get('/vote/:ids', function(req, res){
-//     const { ids } = req.params;
-//     const questions = JSON.parse(
-//         fs.readFileSync("questions.json", {encoding:"utf-8"}
-//         ));
-//     res.send(`
-//             <h1>${questions[ids]["content"]}</h1>
-//             <li>yes: ${questions[ids]["yes"]}</li>
-//             <li>no: ${questions[ids]["no"]}</li>
-//             <a href="/">Go back</a>
-//             `)
-     
-// })
 
 app.listen(9696, function(err){
     if (err) console.log(err)
